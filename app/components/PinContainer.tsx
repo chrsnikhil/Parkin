52c178d3a206d3b2795e58a99160ec2392c8765c
Nikhil <chrsnikhil@gmail.com>
2025-01-26 18:38:45 +0530

    Deployment fix

--- app/components/PinContainer.tsx
+++ app/components/PinContainer.tsx
@@ -65,16 +65,16 @@ export const PinPerspective = ({
     <motion.div className="pointer-events-none w-full h-full flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className="w-full h-full -mt-7 flex-none inset-0">
         <div className="absolute top-0 inset-x-0 flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
+          <Link
+            href={href || "#"}
+            target="_blank"
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
             rel="noreferrer"
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">{title}</span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </Link>
         </div>
 
         <div
@@ -85,63 +85,28 @@ export const PinPerspective = ({
           className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2 w-full h-full"
         >
           <>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Number.POSITIVE_INFINITY,
-                delay: 0,
-              }}
-              className="absolute left-1/2 top-1/2 h-full w-full rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Number.POSITIVE_INFINITY,
-                delay: 2,
-              }}
-              className="absolute left-1/2 top-1/2 h-full w-full rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0,
-                x: "-50%",
-                y: "-50%",
-              }}
-              animate={{
-                opacity: [0, 1, 0.5, 0],
-                scale: 1,
-                z: 0,
-              }}
-              transition={{
-                duration: 6,
-                repeat: Number.POSITIVE_INFINITY,
-                delay: 4,
-              }}
-              className="absolute left-1/2 top-1/2 h-full w-full rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
-            ></motion.div>
+            {[0, 2, 4].map((delay) => (
+              <motion.div
+                key={delay}
+                initial={{
+                  opacity: 0,
+                  scale: 0,
+                  x: "-50%",
+                  y: "-50%",
+                }}
+                animate={{
+                  opacity: [0, 1, 0.5, 0],
+                  scale: 1,
+                  z: 0,
+                }}
+                transition={{
+                  duration: 6,
+                  repeat: Number.POSITIVE_INFINITY,
+                  delay: delay,
+                }}
+                className="absolute left-1/2 top-1/2 h-full w-full rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
+              ></motion.div>
+            ))}
           </>
         </div>
 
@@ -155,4 +120,3 @@ export const PinPerspective = ({
     </motion.div>
   )
 }
-
